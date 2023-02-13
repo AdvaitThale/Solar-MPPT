a7554dbd77f8b6bcf93594b0ce133cc40056ad20
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-02-13 21:08:40 +0530

    Config (#10)

--- MPPT_Port-328P/MPPT_Port-328P.ino
+++ MPPT_Port-328P/MPPT_Port-328P.ino
@@ -17,35 +17,34 @@
   for furthur details on MPPT.
 
   **************************************************************************************************
-                                           ESP32 PINOUT
+                                           328P-U PINOUT
   **************************************************************************************************
 
-                               3V3 |*|                       |*| 5V
-                               GND |*|                       |*| GND
-                               IR  |*| GPIO15         GPIO13 |*| C.START
-                               CUR |*| GPIO02         GPIO12 |*| C.STOP
-                    AMB TEMP / HUM |*| GPIO04         GPIO14 |*| ALARM
-                            L.TRIG |*| GPIO16         GPIO27 |*| P.COUNT
-                            L.ECHO |*| GPIO17         GPIO26 |*|  -
-                              BUZZ |*| GPIO05         GPIO25 |*| RELAY
-                                -  |*| GPIO18         GPIO33 |*| RELAY
-                                -  |*| GPIO19         GPIO32 |*| RELAY
-                               SDA |*| GPIO21         GPIO35 |*| RELAY
-                               RX  |*| GPIO03         GPIO34 |*| PT-100
-                               TX  |*| GPIO01         GPIO39 |*| FLOW
-                               SCL |*| GPIO22         GPIO36 |*| PRES
-                                -  |*| GPIO23           EN   |*|  -
+                              BUZZ |*| D13         D12 |*| C.START
+                               3V3 |*|             D11 |*| 
+                               GND |*| AREF        D10 |*| 
+                               CUR |*| A0           D9 |*| RELAY
+                                   |*| A1           D8 |*| RELAY
+                         PT-100(I) |*| A2           D7 |*| 
+                         PT-100(O) |*| A3           D6 |*|  -
+                               SDA |*| A4           D5 |*| CURRENT
+                               SCL |*| A5           D4 |*| AMB TEMP / HUM
+                                -  |*| A6           D3 |*| 
+                                -  |*| A7           D2 |*| 
+                                   |*| 5V          GND |*| 
+                                   |*| RST         RST |*|  -
+                                   |*| GND         RX0 |*|  TX
+                                -  |*| VIN         TX0 |*|  RX
 
   ***************************************************************************************************
 
 
   # Standardised System Variables
 
-  BYTCT     Cycle Start/Cycle Stop       ON/OFF
   BYTA      Alarm                        ON/OFF
-  BYTN      Count                        N(Discrete Number)
+  BYTB      Battery                      Percentage
   BYTO      Outdoor Temperature          C(Degree Celcius)
-  BYTI      Current (AC/DC) upto 5A      A(Ampere)
+  BYTI      Current (AC/DC) upto 20A      A(Ampere)
   BYTT      Indoor Temperature           C(Degree Celcius)
   BYTL      Load Power                   W(Watts)
   BYTP      Power                        W(Watts)
@@ -55,31 +54,27 @@
 */
 
 #include <math.h>   // Math Functions
-#include <ACS712.h> // ACS712 Functions
-#include <dht.h>    // Ambient Temperature/Humidity Functions
+//#include <ACS712.h> // ACS712 Functions
+#include <DHT.h>    // Ambient Temperature/Humidity Functions
 #include <Wire.h>   // For communication with I2C devices
 #include <LiquidCrystal_I2C.h>
 
 #define CUR 2
-#define TRIG 16
-#define ECHO 17
 #define DHTPIN 4
 #define BUZZ 11
 #define dt 2000
 
 long DUR;
-float BYTA, BYTT, BYTO, BYTL, BYTP, BYTS, BYTR, BYTI, BYTN, BYTH, BYTX, THLD1, THLD2;
+float BYTA, BYTB, BYTI, BYTO, BYTT, BYTL, BYTP, BYTS, BYTH, THLD1, THLD2;
 byte charge[13] = {0x04, 0x0C, 0x1C, 0x1F, 0x1F, 0x07, 0x06, 0x04};
 
-dht DHT;
-ACS712 sensor(ACS712_05B, 2);
+//dht DHT;
+//ACS712 sensor(ACS712_05B, 2);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup()
 {
   pinMode(CUR, INPUT);          // ADC Pin for ACS712
-  pinMode(TRIG, OUTPUT);        // TRIGGER Pin of HC-SR05 to Write
-  pinMode(ECHO, INPUT);         // ECHO Pin of HC-SR05 to Read
   pinMode(BUZZ, OUTPUT);        // Buzzer Pin for Beeps
   pinMode(DHTPIN, INPUT);       // DHT11 Pin as Input
   Serial.begin(9600);           // Compensated Baud to 9600 for RS-232
@@ -95,28 +90,30 @@ void setup()
 
 void loop()
 {
-  lcd.setCursor(0, 0); //(C, R)
-  lcd.print("SOLAR:");
-  lcd.setCursor(6, 0);
-  lcd.print("89W");
-  lcd.setCursor(11, 0);
-  lcd.print("LOAD:");
-  lcd.setCursor(16, 0);
-  lcd.print("22W");
-  lcd.setCursor(0, 1);
-  lcd.print("BAT:");
-  lcd.createChar(13, charge);
-  lcd.setCursor(5, 1);
-  lcd.write(byte(13));
-  lcd.setCursor(6, 1);
-  lcd.print("67%");
-  lcd.setCursor(9, 1);
-  lcd.print("2H 21M");
+  //  lcd.noBacklight();
+  //  lcd.setCursor(0, 0); //(C, R)
+  //  lcd.print("SOLAR:");
+  //  lcd.setCursor(6, 0);
+  //  lcd.print("89W"); // BYTS
+  //  lcd.setCursor(11, 0);
+  //  lcd.print("LOAD:");
+  //  lcd.setCursor(16, 0);
+  //  lcd.print("22W"); // BYTL
+  //  lcd.setCursor(0, 1);
+  //  lcd.print("BAT:");
+  //  lcd.createChar(13, charge);
+  //  lcd.setCursor(5, 1);
+  //  lcd.write(byte(13));
+  //  lcd.setCursor(6, 1);
+  //  lcd.print("67%"); //BYTB
+  //  lcd.setCursor(10, 1);
+  //  lcd.print("2H 21M");
+  alarm();
 }
 
 void initial()
 {
-  lcd.setCursor(3, 0);
+  lcd.setCursor(6, 0);
   lcd.print("MPPT");
   lcd.setCursor(0, 1);
   lcd.print("----------------");
@@ -126,10 +123,32 @@ void initial()
   digitalWrite(BUZZ, LOW);
   tone(BUZZ, 4093);
   delay(200);
-  noTone();
+  noTone(BUZZ);
   digitalWrite(BUZZ, HIGH);
   delay(100);
   digitalWrite(BUZZ, LOW);
   delay(dt);
   lcd.clear();
+}
+
+void alarm() {
+  Serial.print("ALERT >> ");
+  Serial.println("THRESHOLD EXCEEDED !!");
+  lcd.noBacklight();
+  lcd.setCursor(3, 0);
+  lcd.print("THRESHOLD");
+  lcd.setCursor(3, 1);
+  lcd.print("EXCEEDED!!");
+  tone(BUZZ,3000);
+  delay(500);
+  lcd.backlight();
+  tone(BUZZ,500);
+  delay(500);
+  lcd.noBacklight();
+  lcd.setCursor(3, 0);
+  lcd.print("THRESHOLD");
+  lcd.setCursor(3, 1);
+  lcd.print("EXCEEDED!!");
+  Serial.println("Restart the Device...");
+  Serial.println(" ");
 }
