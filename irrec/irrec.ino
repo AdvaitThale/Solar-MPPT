3a4cee6b86925897b2050edc0db7fd26bdde0759
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-02-20 18:23:10 +0530

    Config (#16)

--- irrec/irrec.ino
+++ irrec/irrec.ino
@@ -1,24 +1,47 @@
-#include <Arduino.h>
-#include <IRremote.hpp>
 
+//#include <Arduino.h>
+//#include <IRremote.hpp>
 //#include <IRremote.h>
+#include "IRremote.hpp"
 
-int recv_pin = 15;
+#define receiver 15 // IR receiver, Connect the Y pin of the Keyes IR sensor with the GPIO 26 of ESP32 
 
-IRrecv irrecv(recv_pin);
-decode_results results;
+int GLED = 13; // LED Pin
+int BUZZ = 4; // BUZZER Pin
+
+IRrecv irrecv(receiver);           // create instance of 'irrecv'
+decode_results results;            // create instance of 'decode_results'
 
 void setup() {
   Serial.begin(115200);
-  irrecv.enableIRIn();
-//  pinMode(out, OUTPUT);
+  Serial.println("IR Receiver -> ");
+  irrecv.enableIRIn(); // Start the receiver
+  pinMode(GLED, OUTPUT);
+  pinMode(BUZZ, OUTPUT);
+  digitalWrite(GLED, LOW);
+  digitalWrite(BUZZ, LOW);
 }
 
+
 void loop() {
-  if (irrecv.decode(&results)) {
+  if (irrecv.decode(&results)) // have we received an IR signal?
+  {
     Serial.println(results.value, HEX);
-    irrecv.resume();
+    //    translateIR();
+    if (results.value == 0x23DCFD01) {
+      digitalWrite(GLED, !digitalRead(GLED));
+      delay(100);
+    }
+
+    else if (results.value == 0x639CFD01) {
+      digitalWrite(BUZZ, !digitalRead(BUZZ));
+      delay(100);
+    }
+
+    else {
+      Serial.println("Invalid Command!!");
+      delay(100);
+    }
+    irrecv.resume(); // receive the next value
   }
-  delay(100);
-  0x23DCFD01
 }
