95b7ab19d967c09238c1765a8df7cc7d8e40f6a5
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-02-23 01:30:39 +0530

    Config (#18)

--- irrec/irrec.ino
+++ irrec/irrec.ino
@@ -2,6 +2,34 @@
   Author: Advait Thale
 
 
+  IR Remote Keypad Hex Map:
+  
+        +------------------+              
+        |              DC  |
+        |                  |
+        |  92    93    CC  |
+        |  8E    8F    C8  |
+        |  8A    8B    C4  |
+        |  CF    87    82  |
+        |                  |
+        |  80    9C    85  |
+        |  81  8C  DD  86  |
+        |                  |
+        |        CA        |
+        |  88 99 CE C1 C5  |
+        |        D2        |
+        |                  |
+        |  DA          D6  |
+        |      98  95      |
+        |                  |
+        |  CD    91    D0  |
+        |  8D  97  C3  83  |
+        |                  |
+        |  89  84  D9  96  |
+        |                  |
+        |     hathw@y      |
+        +------------------+
+
 
   IR Receiver Pinout:
   -------------
@@ -16,12 +44,12 @@
 
 const byte IR_RECEIVE_PIN = 2; // IR receiver
 
-int ILCD = 11;   
+int ILCD = 11;
 int GLED = 12;     // LED Pin
 int BUZZ = 4;      // BUZZER Pin
 
-           // create instance of 'irrecv'
-           // create instance of 'decode_results'
+// create instance of 'irrecv'
+// create instance of 'decode_results'
 
 void setup() {
   Serial.begin(115200);
@@ -39,7 +67,7 @@ void setup() {
 void loop() {
   if (IrReceiver.decode()) {
     Serial.println(IrReceiver.decodedIRData.command, HEX);
-   
+
     if (IrReceiver.decodedIRData.command == 0xDC) {
       digitalWrite(ILCD, !digitalRead(ILCD));
       delay(100);
@@ -48,7 +76,7 @@ void loop() {
       digitalWrite(BUZZ, !digitalRead(BUZZ));
       delay(100);
     }
-     else if (IrReceiver.decodedIRData.command == 0x84) {
+    else if (IrReceiver.decodedIRData.command == 0x84) {
       digitalWrite(GLED, !digitalRead(GLED));
       delay(100);
     }
