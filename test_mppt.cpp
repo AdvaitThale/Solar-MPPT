9abf6c0717d13a2a570f7a4e957bab74664efa61
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-05-13 22:58:43 +0530

    Config (#29)

---
+++ test_mppt.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define LED 2
+// #define LED 2
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  // pinMode(LED, INPUT);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  Serial.print("LED ON");
+  delay(500);
+  digitalWrite(LED, LOW);
+  Serial.print("LED OFF");
+  delay(500);
+}
