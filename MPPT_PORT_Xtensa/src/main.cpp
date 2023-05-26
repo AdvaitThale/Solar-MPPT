8607ad04b6e0edf6231ed72f93e4e9c389010c51
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-05-26 22:16:14 +0530

    Bugs & Fixes (#34)

--- MPPT_PORT_Xtensa/src/main.cpp
+++ MPPT_PORT_Xtensa/src/main.cpp
@@ -45,10 +45,10 @@
 
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include <IRremote.h>
+// #include <IRremote.h>
 #include <ACS712.h>
-#include <WiFi.h>
-#include <WebServer.h>
+// #include <WiFi.h>
+// #include <WebServer.h>
 
 // Local SSID
 #define WIFI_SSID "MPPT Rev 2.3"
@@ -59,10 +59,10 @@
 // #define WIFI_PASSWORD "amhipuneri"
 
 /* Put IP Address details */
-IPAddress local_ip(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-WebServer server(80);
+// IPAddress local_ip(192, 168, 1, 1);
+// IPAddress gateway(192, 168, 1, 1);
+// IPAddress subnet(255, 255, 255, 0);
+// WebServer server(80);
 
 #define CUR 13     // ACS712
 #define MOSFET_1 2 // MOSFET Driver Opto-Isolator 2 // Built-In LED Pin
@@ -90,9 +90,7 @@ ACS712 sensor(ACS712_05B, 13);
 void greetings();
 void SOLAR_INFO();
 void BATTERY_INFO();
-void handle_OnConnect();
-void handle_NotFound();
-String SendHTML();
+
 
 void tone(byte PIN, int FREQ)
 {
@@ -120,35 +118,22 @@ void setup()
   pinMode(IREC, INPUT);      // IR Receiver VS1838B
   lcd.init();                // LCD Initialise
   lcd.backlight();           // LCD Backlight ON
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD); // Set it to NULL, if you don’t want to set a password.
-  WiFi.softAPConfig(local_ip, gateway, subnet);
+  // WiFi.mode(WIFI_AP);
+  // WiFi.softAP(WIFI_SSID, WIFI_PASSWORD); // Set it to NULL, if you don’t want to set a password.
+  // WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
   Serial.print("Connecting ");
   greetings(); // Device Start Configuration
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-  server.begin();
+  
 }
 
 void loop()
 {
-  server.handleClient();
+
   SOLAR_INFO();
   delay(dt);
   BATTERY_INFO();
   delay(dt);
-}
-
-void handle_OnConnect()
-{
-  loop();
-  server.send(200, "text/html", SendHTML(SOL_VOLTAGE, BAT_CURRENT, PID_TEMP));
-}
-
-void handle_NotFound()
-{
-  server.send(404, "text/plain", "Device is Not Connected to ESP32!!");
 }
 
 void greetings()
@@ -178,7 +163,7 @@ void SOLAR_INFO()
   lcd.home();
   SOL_VOL = analogRead(SVOL);
   SOL_VOLTAGE = SOL_VOL * (4.0 / 4096) * 10;
-  BAT_CURRENT = (sensor.getCurrentAC() / 1000);
+  BAT_CURRENT = (sensor.getCurrentDC() / 1000);
   RAW_TEMP = analogRead(LM);           // Read LM ADC
   PID_TEMP = ((RAW_TEMP * 1.22) / 10); // Convert ADC value to equivalent voltage and divide since LM35 gives output of 10mv/°C*/
   lcd.setCursor(0, 0);
@@ -219,7 +204,7 @@ void BATTERY_INFO()
   lcd.home();
   BAT_VOL = analogRead(BVOL);
   BAT_VOLTAGE = BAT_VOL * (4.0 / 4096);
-  BAT_CURRENT = (sensor.getCurrentAC() / 1000);
+  BAT_CURRENT = (sensor.getCurrentDC() / 1000);
   RAW_TEMP = analogRead(LM);           // Read LM ADC
   PID_TEMP = ((RAW_TEMP * 1.22) / 10); // Convert ADC value to equivalent voltage and divide since LM35 gives output of 10mv/°C*/
   lcd.setCursor(0, 0);
@@ -251,54 +236,7 @@ void BATTERY_INFO()
   Serial.print(PID_TEMP);
   Serial.println(" °C  |");
 }
-String SendHTML(float SOL_VOLTAGE, float BAT_CURRENT, float PID_TEMP, float BAT_VOLTAGE)
-{
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<meta content=\"Advait Thale\" name=\"author\"><meta content=\"1\" http-equiv=\"refresh\">\n";
-  ptr += "<title>MPPT Monitor</title>\n";
-  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr += "footer {text-align: center;text-decoration: none;position: fixed;left: 0;bottom: 0;width: 100%;height: 3%;padding: 2px;background-color: #00004d;color: #f2f2f2;}\n";
-  ptr += "</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
-  ptr += "<div id=\"webpage\">\n";
-  ptr += "<h1>MPPT MASTER DASHBOARD</h1>\n";
-  if (PID_TEMP >= 50)
-  {
-    ptr += "<p>TEMP. OVERLOAD<svg width=\"170\" height=\"15\"><rect x=\"15\" y=\"5\" rx=\"7\" ry=\"7\" width=\"20\" height=\"10\" style=\"fill: #e60000\"/></svg> </p>";
-  }
-  else if (PID_TEMP > 25 && PID_TEMP < 50)
-  {
-    ptr += "<p>TEMP. OVERLOAD<svg width=\"170\" height=\"15\"><rect x=\"15\" y=\"5\" rx=\"7\" ry=\"7\" width=\"20\" height=\"10\" style=\"fill: #ff8800\"/></svg> </p>";
-  }
-  else
-  {
-    ptr += "<p>TEMP. OVERLOAD<svg width=\"170\" height=\"15\"><rect x=\"15\" y=\"5\" rx=\"7\" ry=\"7\" width=\"20\" height=\"10\" style=\"fill: #009933\"/></svg> </p>";
-  }
 
-  ptr += "<p> BATTERY\n </p>";
-  ptr += "<p> Voltage: ";
-  ptr += (float)BAT_VOLTAGE;
-  ptr += "</p>";
-  ptr += "<p> Current: ";
-  ptr += (float)BAT_CURRENT;
-  ptr += "</p>";
-  ptr += "<p> BATTERY TEMP.: ";
-  ptr += (float)PID_TEMP;
-  ptr += "</p>";
-  ptr += "<p> SOLAR\n </p>";
-  ptr += "<p> Voltage: ";
-  ptr += (float)SOL_VOLTAGE;
-  ptr += "</p>";
-  ptr += "</div>\n";
-  ptr += "<footer>MPPT MASTER DASHBOARD -> @Author: Advait Thale</footer>";
-  ptr += "</body>\n";
-  ptr += "</html>\n";
-  return ptr;
-}
 // void IR_MOSFET_Control()
 // {
 //   if (IrReceiver.decode())
