7b061927bd3e1a9a4781cbde5063a71911929a8e
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-11-27 22:18:12 +0530

    Bugs & Fixes (#36)

--- vol/vol.ino
+++ vol/vol.ino
@@ -1,29 +1,37 @@
-#include <Arduino.h>
-#include <WiFiMulti.h>
+//#include <Arduino.h>
+//#include <WiFiMulti.h>
+//
 
-
-#define WIFI_SSID "MPPT Rev 2.3"
-#define WIFI_PASSWORD "Mppt_N3t"
-
-WiFiMulti wifiMulti;
+//#define WIFI_SSID "MPPT Rev 2.3"
+//#define WIFI_PASSWORD "Mppt_N3t"
+//
+//WiFiMulti wifiMulti;
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode(13, OUTPUT);
 //  pinMode(LED_BUILTIN, OUTPUT);
-
-  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting");
-
-  while (wifiMulti.run() != WL_CONNECTED)
-  {
-    delay(100);
-    Serial.print(".");
-  }
-  Serial.println("Connected!!");
+//
+//  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+//  Serial.print("Connecting");
+//
+//  while (wifiMulti.run() != WL_CONNECTED)
+//  {
+//    delay(100);
+//    Serial.print(".");
+//  }
+//  Serial.println("Connected!!");
 }
 
 void loop()
 {
+//  for(int i = 0; i<n; i++){
+//    analogWrite(13, i);
+//  }
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
 //  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
 }
